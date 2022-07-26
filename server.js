4972d333020dbda76ff92a5943419a9f1fa03f60
Shmuel Hoffman <snyh212@gmail.com>
2022-07-25 20:14:27 -0700

    added psuedocode and express, fs, & path require paths

---
+++ server.js
@@ -0,0 +1,12 @@
+//dependancies
+//read and write const
+//set up server
+//static middlewhere
+//get route for db.json
+//POST route
+//(delete) route
+//HTML routes
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");

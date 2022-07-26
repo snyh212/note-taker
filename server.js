b6c3b63477479869cc031aa8be312062acb49b0d
Shmuel Hoffman <snyh212@gmail.com>
2022-07-25 20:52:37 -0700

    added delete function

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-//dependancies/
-//read and write const/
-//set up server/
-//static middlewhere/
-//get route for db.json
-//POST route
-//(delete) route
-//HTML routes
-
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -16,7 +7,7 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3010;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -42,6 +33,23 @@ app.post("/api/notes", function(req, res) {
     })
 });
 
+app.delete("/api/notes/:id", function(req, res) {
+    const idDelete = parseInt(req.params.id);
+    readFileAsync("./develop/db/db.json", "utf-8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        const newNotes = []
+        for (let i=0; i<notes.length; i++) {
+            if(idDelete !== notes[i].id) {
+                newNotes.push(notes[i])
+            }
+        }
+        return newNotes;
+    }).then(function(notes) {
+        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+        res.send("success!")
+    })
+});
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
 });
@@ -53,5 +61,5 @@ app.get("/", function(req, res) {
 });
 
 app.listen(PORT, function() {
-    console.log("App listerning on PORT "+PORT);
+    console.log("App listerning on PORT http://localhost:"+PORT);
 });
